7a7e2a8d5ab9f659d5bde1f234a16c77a9d1b244
desendoo <desendoimanuel46@gmail.com>
2024-11-01 14:04:25 +0700

    Async Replication Config to Multiple Instances

--- postgresql/init/primary_init.sql
+++ postgresql/init/primary_init.sql
@@ -13,8 +13,9 @@ CREATE PUBLICATION my_publication FOR TABLE sample_table;
 -- Enable extended display mode
 \x
 
--- Create a logical replication slot for the subscription\
+-- Create a logical replication slot for the subscription
 SELECT * FROM pg_create_logical_replication_slot('my_subscription', 'pgoutput');
+SELECT * FROM pg_create_logical_replication_slot('my_subscription2', 'pgoutput');
 SELECT * FROM pg_replication_slots;
 
 -- Grant permissions to replication user
--- postgresql/tests/test_replication.sh
+++ postgresql/tests/test_replication.sh
@@ -27,13 +27,12 @@ check_data() {
   run_query $host $port $user $db "SELECT * FROM sample_table;"
 }
 
-# Function to insert data into the primary database
+# Function to insert data into the database
 insert_data_primary() {
   local host=$1
   local port=$2
   local user=$3
   local db=$4
-  echo "Inserting data into primary..."
   local query="INSERT INTO sample_table (data) VALUES ('Replication test ' || (SELECT COALESCE(MAX(id), 0) + 1 FROM sample_table));"
   run_query $host $port $user $db "$query"
 }
@@ -44,15 +43,28 @@ check_replication_status() {
   local port=$2
   local user=$3
   local db=$4
-  echo "Checking the subscription slot in primary..."
+  echo "Checking the subscription slot on ${host}..."
   psql -h $host -p $port -U $user -d $db -xc "SELECT * FROM pg_replication_slots"
-  echo "Checking the replication status in primary..."
+  echo "Checking the replication status on ${host}..."
   psql -h $host -p $port -U $user -d $db -xc "SELECT * FROM pg_stat_replication"
+}
+
+# Function to check subscription status
+check_subscription_status() {
+  local host=$1
+  local port=$2
+  local user=$3
+  local db=$4
+  echo "Checking the subscription status on ${host}..."
+  psql -h $host -p $port -U $user -d $db -xc "SELECT * FROM pg_subscription"
 }
 
 # Check the standby initially
 echo "Checking standby..."
 set_pgpassword $PG_STANDBY_PASSWORD
+check_data $PG_STANDBY_HOST $PG_STANDBY_DB_PORT $PG_STANDBY_USER $PG_STANDBY_DB
+echo "Checking standby2..."
+export PG_STANDBY_HOST=postgresql-pg_standby2-1
 check_data $PG_STANDBY_HOST $PG_STANDBY_DB_PORT $PG_STANDBY_USER $PG_STANDBY_DB
 
 # Check the primary initially
@@ -61,6 +73,7 @@ set_pgpassword $PG_PRIMARY_PASSWORD
 check_data $PG_PRIMARY_HOST $PG_PRIMARY_DB_PORT $PG_PRIMARY_USER $PG_PRIMARY_DB
 
 # Insert data into the primary to test replication
+echo "Inserting data into primary..."
 insert_data_primary $PG_PRIMARY_HOST $PG_PRIMARY_DB_PORT $PG_PRIMARY_USER $PG_PRIMARY_DB
 insert_data_primary $PG_PRIMARY_HOST $PG_PRIMARY_DB_PORT $PG_PRIMARY_USER $PG_PRIMARY_DB
 
@@ -73,7 +86,18 @@ sleep 1
 # Check the standby again
 set_pgpassword $PG_STANDBY_PASSWORD
 echo "Data in standby after insertion from primary:"
+export PG_STANDBY_HOST=postgresql-pg_standby-1
 check_data $PG_STANDBY_HOST $PG_STANDBY_DB_PORT $PG_STANDBY_USER $PG_STANDBY_DB
+echo "Data in standby2 after insertion from primary:"
+export PG_STANDBY_HOST=postgresql-pg_standby2-1
+check_data $PG_STANDBY_HOST $PG_STANDBY_DB_PORT $PG_STANDBY_USER $PG_STANDBY_DB
+
+echo "Checking subscription status on standby..."
+export PG_STANDBY_HOST=postgresql-pg_standby-1
+check_subscription_status $PG_STANDBY_HOST $PG_STANDBY_DB_PORT $PG_STANDBY_USER $PG_STANDBY_DB
+echo "Checking subscription status on standby2..."
+export PG_STANDBY_HOST=postgresql-pg_standby2-1
+check_subscription_status $PG_STANDBY_HOST $PG_STANDBY_DB_PORT $PG_STANDBY_USER $PG_STANDBY_DB
 
 # Check replication status
 set_pgpassword $PG_PRIMARY_PASSWORD
